9e45b7233b743dc498b603e6bef2b1c7f4f02058
lalmendra <lucascontatoprincipal@gmail.com>
2024-12-16 10:28:27 -0300

    primeiro commit, fiz tutorial até antes de iniciar a seção 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'],
+    style: 'normal', 
+});
+
+export const lusitana = Lusitana({ 
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    style: 'normal', 
+});
